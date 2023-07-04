e20b25b7d23f389fc69da9f54a596162da0ce256
agathaappb <agatha.alana@hotmail.com>
2023-07-04 18:25:10 -0300

    Implementando estados
    -> implementando estados
    -> evento de click
    -> expansão do padding e recolhendo conforme click e estado

--- app/src/main/java/com/example/composecodelabs/MainActivity.kt
+++ app/src/main/java/com/example/composecodelabs/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,10 +32,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if(expanded.value) 18.dp else 0.dp
+
     Surface(color = MaterialTheme.colorScheme.secondary, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)){
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
-                modifier = Modifier.weight(1f)) {
+                modifier = Modifier.weight(1f).padding(bottom = extraPadding)) {
                 Text(
                     text = "Hello,"
                 )
@@ -41,8 +46,8 @@ fun Greeting(name: String) {
                     text = "$name!"
                 )
             }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+            Button(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show less" else "Show more")
 
             }
         }
