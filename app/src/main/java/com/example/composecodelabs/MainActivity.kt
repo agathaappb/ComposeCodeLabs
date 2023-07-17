d768ab691057ff0b1962925926bb9102a75def91
agathaappb <agatha.alana@hotmail.com>
2023-07-17 17:02:59 -0300

    Adicionando um novo componente

--- app/src/main/java/com/example/composecodelabs/MainActivity.kt
+++ app/src/main/java/com/example/composecodelabs/MainActivity.kt
@@ -3,17 +3,23 @@ package com.example.composecodelabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,47 +39,94 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if(expanded.value) 18.dp else 0.dp
+    val extraPadding = if(expanded.value)4.dp else 0.dp
 
     Surface(color = MaterialTheme.colorScheme.secondary, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)){
-        Row(modifier = Modifier.padding(24.dp)) {
+        Row(modifier = Modifier.padding(20.dp)) {
             Column(
-                modifier = Modifier.weight(1f).padding(bottom = extraPadding)) {
+                modifier = Modifier
+                    .weight(1f)
+                    ) {
                 Text(
                     text = "Hello,"
                 )
                 Text(
                     text = "$name!"
                 )
+
+                if (expanded.value){
+                    Text(
+                        text = "You can see now! o/", modifier = Modifier.padding(10.dp))
+                }
+
+
             }
             Button(onClick = { expanded.value = !expanded.value }) {
                 Text(if (expanded.value) "Show less" else "Show more")
-
             }
+
+
+
         }
     }
 
+}
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit,modifier:Modifier = Modifier){
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Column(modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Welcome to the Basic Codelabs!")
+        Button(modifier = Modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked) {
+            Text(text = "Continue")
+        }
+
+    }
 }
 
 @Composable
-private fun MyApp(modifier: Modifier = Modifier,
-                  names: List<String> = listOf("World", "Compose")) {
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(modifier) {
-            for (name in names){
-                Greeting(name)
-            }
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    Surface(modifier) {
+        if (shouldShowOnboarding){
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        }else{
+            Greetings()
         }
     }
 }
+
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+
+
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    ComposeCodeLabsTheme {
+        OnboardingScreen(onContinueClicked = { })
+    }
+}
+
 
 @Preview(name= "Primeira implementação - Compose", showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     ComposeCodeLabsTheme {
-        MyApp()
+        MyApp(Modifier.fillMaxSize())
     }
 }
